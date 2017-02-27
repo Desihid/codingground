f1514029e5823445c10d9a8f5ca2afd7e5de821b
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-02-27 09:25:08 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,9 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Guntur Desi Hidayani";
+        String kota = "Jepara";
+        
+        System.out.println("Nama : " + nama + "\nKota : " + kota);
+     }
+}
