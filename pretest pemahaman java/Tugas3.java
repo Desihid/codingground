6d681111242075470a86f5cd0a58e00fbe0c40b5
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-02-27 09:41:44 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas3.java
@@ -0,0 +1,11 @@
+public class Tugas3{
+
+    public static void TampilAngka(){
+        int nom = 3;
+        System.out.println("Angka yang anda masukkan adalah angka " + nom);
+    }
+
+    public static void main(String []args){
+        Tugas3.TampilAngka();
+    }
+}
