d403f4c1b880f738fbbdaf7ba34566b2273d33f8
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-03-05 11:47:05 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas4.java
@@ -0,0 +1,14 @@
+public class Tugas4{
+
+    public static void main(String []args){
+        int ang = 1;
+        int hasil = Penjumlahan(ang);
+        System.out.println("Penjumlahan " + ang + " + 2 = " + hasil);
+     }
+     
+    public static int Penjumlahan(int x) {
+        int angka = x;
+        
+        return angka + 2; 
+   }
+}
