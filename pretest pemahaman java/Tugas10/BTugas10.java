057d1385ca057a4562a7fd175a331fcd76ead55a
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-03-05 14:43:13 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas10/BTugas10.java
@@ -0,0 +1,25 @@
+public class BTugas10{
+    
+	String nama, alamat;
+	int umur;
+
+	void setNama(String n){
+		nama = n;
+	}
+	void setUmur(int u){
+		umur = u;
+	}
+	void setAlamat(String a){
+		alamat = a;
+	}
+
+	String getNama(){
+		return nama;
+	}
+	int getUmur(){
+		return umur;
+	}
+	String getAlamat(){
+		return alamat;
+	}
+}
