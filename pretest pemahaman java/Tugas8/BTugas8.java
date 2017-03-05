9a254bc7a820ec016fc27a81f66e40223ca6a15d
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-03-05 14:13:17 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas8/BTugas8.java
@@ -0,0 +1,10 @@
+public class BTugas8{
+	
+	String nama = "Guntur Desi Hidayani";
+	String alamat = "Senenan Jepara";
+	int umur = 20;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
