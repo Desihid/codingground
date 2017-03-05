31161e402ae741a4118607d580ccc078878de418
Guntur Desi Hidayani <debie07sis@gmail.com>
2017-03-05 15:01:30 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas11/MTugas11.java
@@ -0,0 +1,9 @@
+public class MTugas11 extends BTugas11{
+    
+	String nim = "141240000234";
+	String jurusan = "Teknik Informatika";
+
+	void CetakProfilMhs(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
